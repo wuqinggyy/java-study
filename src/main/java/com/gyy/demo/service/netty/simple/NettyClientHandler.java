3624a1d049172505e30d99da95333dd24b412a1f
wuqinggyy <632737078@qq.com>
2021-06-04 00:17:56 +0800

    啊打
    代码提交；

--- src/main/java/com/gyy/demo/service/netty/simple/NettyClientHandler.java
+++ src/main/java/com/gyy/demo/service/netty/simple/NettyClientHandler.java
@@ -1,4 +1,4 @@
-package com.gyy.demo.service.netty.netty;
+package com.gyy.demo.service.netty.simple;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
@@ -8,23 +8,25 @@ import io.netty.util.CharsetUtil;
 
 public class NettyClientHandler extends ChannelInboundHandlerAdapter {
 
-    //当通道就绪就会触发
+    //当通道就绪就会触发该方法
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        System.out.println("client" + ctx);
+        System.out.println("client " + ctx);
         ctx.writeAndFlush(Unpooled.copiedBuffer("hello, server: (>^ω^<)喵", CharsetUtil.UTF_8));
     }
 
     //当通道有读取事件时，会触发
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+
         ByteBuf buf = (ByteBuf) msg;
         System.out.println("服务器回复的消息:" + buf.toString(CharsetUtil.UTF_8));
-        System.out.println("服务器的地址: " + ctx.channel().remoteAddress());
+        System.out.println("服务器的地址： "+ ctx.channel().remoteAddress());
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
         ctx.close();
     }
 }
