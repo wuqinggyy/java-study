de9ef67e02368b6d5789544261f2f5c95eaf30ae
gongyy <632737078@qq.com>
2021-07-02 16:44:37 +0800

    代码提交；

--- src/main/java/com/gyy/demo/service/Singleton/Solutionxxx.java
+++ src/main/java/com/gyy/demo/service/Singleton/Solutionxxx.java
@@ -1,36 +1,26 @@
 package com.gyy.demo.service.Singleton;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
-
-import org.springframework.data.relational.core.sql.In;
-
 public class Solutionxxx {
 
-    public int[] intersect(int[] nums1, int[] nums2) {
+    public void rotate(int[][] matrix) {
 
-        Set<Integer> list = new HashSet<>();
-        for (int i = 0; i < nums1.length; i++) {
-            for (int j = 0; j < nums2.length; j++) {
-                if (nums1[i] == nums2[j]) {
-                    list.add(nums1[i]);
-                }
-            }
-        }
-        int[] intArray = new int[list.size()];
-        for (int i = 1; i < list.size(); i++) {
-            intArray[i] = list.;
-        }
-        return intArray;
+        int len = matrix.length;
 
     }
 
-    public int[] intersect(int[] nums1, int[] nums2) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        map.getOrDefault()
+
+    public void reverseString(char[] s) {
+
+        int len = s.length;
+
+        for (int i = 0; i < len / 2; i++) {
+            char tmp = s[i];
+            s[i] = s[len - i + 1];
+            s[len - i + 1] = tmp;
+        }
+
+
     }
+}
 
 }
